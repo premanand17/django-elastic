e85ca0fac828f553b3965a300607a921b6cf21ac
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2014-12-23 11:15:10 +0000

    First commit

---
+++ django_template/local_apps/bands/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+
+from db.models import Feature, Featureloc, Featureprop, Cv, Cvterm
+
+import logging
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+def cvlist(request):
+    cv_list = Cv.objects.all()
+    context = {'cv_list': cv_list}
+    return render(request, 'bands/cvlist.html', context)
+
+# 60 mins cache
+@cache_page(60 * 60)
+def cytobands(request, org):
+    #bands = Featureloc.objects.getCytoBands(org)
+
+    srcfeatureIds = Featureloc.objects.getSrcFeatures(org)
+    srcfeatures = Feature.objects.filter(featureloc_srcfeature=srcfeatureIds).order_by('feature_id')
+
+    cv = Cv.objects.get(name="DIL")
+    cvtermDIL = Cvterm.objects.filter(cv=cv)
+
+    context = {'srcfeatures': srcfeatures, 
+               'org':org,
+               'cvtermDIL':cvtermDIL }
+    return render(request, 'bands/bands.html', context, content_type='text/html')
+
+# 60 mins cache
+@cache_page(60 * 60)
+def cytobands2(request, org):
+    bands = Featureloc.objects.getCytoBands(org)
+    srcfeatureIds = bands.distinct('srcfeature_id')
+    srcfeatures = Feature.objects.filter(featureloc_srcfeature=srcfeatureIds).order_by('feature_id')
+
+    cv = Cv.objects.get(name="DIL")
+    cvtermDIL = Cvterm.objects.filter(cv=cv)
+
+    context = {'bands': bands, 'srcfeatures': srcfeatures, 'org':org, 'cvtermDIL':cvtermDIL}
+    return render(request, 'bands/bands2.html', context)
+
