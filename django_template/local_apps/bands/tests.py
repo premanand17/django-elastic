bdf0f6b98930b0066f10a2eb0856b12219870f12
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-08 13:15:22 +0000

    db tests

--- django_template/local_apps/bands/tests.py
+++ django_template/local_apps/bands/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class BandsViewsTestCase(TestCase):
+    """Load fixture"""
+    fixtures = ['db2.json']
+
+    def test_index(self):
+        resp = self.client.get('/bands/cached/human_GRCh38/')
+        self.assertEqual(resp.status_code, 200)
+
+
