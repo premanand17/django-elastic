624633bfc72733dd757aaff1a59a61ed6a614947
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-09 11:54:55 +0000

    add to test

--- django_template/local_apps/bands/tests/tests_pages.py
+++ django_template/local_apps/bands/tests/tests_pages.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
+from django.test.utils import setup_test_environment
+
 
 class BandsViewsTestCase(TestCase):
     """Load fixture"""
     fixtures = ['db2.json']
+    setup_test_environment()
 
     def test_index(self):
         resp = self.client.get('/bands/cached/human_GRCh38/')
         self.assertEqual(resp.status_code, 200)
+
+        if resp.context is not None:  # if none it is cached
+            #print(resp.context['srcfeatures'])
+            self.assertEqual(resp.context['org'], 'human_GRCh38')
+            self.assertTrue('srcfeatures' in resp.context)
+            feature = resp.context['srcfeatures'][0]
+            self.assertTrue(hasattr(feature, 'uniquename'))
 
 
