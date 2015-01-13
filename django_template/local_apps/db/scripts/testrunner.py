1a934532ba52f2fe6087921dafee67a507f919ee
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-13 15:00:53 +0000

    add link

--- django_template/local_apps/db/scripts/testrunner.py
+++ django_template/local_apps/db/scripts/testrunner.py
@@ -2,7 +2,9 @@ from django.test.runner import DiscoverRunner
 from django.conf import settings
 from django.db import connection
 
+# see:
 # http://www.caktusgroup.com/blog/2010/09/24/simplifying-the-testing-of-unmanaged-database-models-in-django/
+# https://www.marshut.net/krupvs/how-to-test-an-application-that-s-using-a-legacy-database.html
 class ManagedModelTestRunner(DiscoverRunner):
     """
     DiscoverRunner subclass that creates un-managed tables from a file containing
