356bc9a6c8c74496c891d1e4a5daf9e25fad0c31
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-20 16:28:21 +0000

    update url and add wildcard

--- django_template/local_apps/es/urls.py
+++ django_template/local_apps/es/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from es import views
 
 urlpatterns = patterns( 'es',
-    url(r'^$', views.search, name='search'),
-    url(r'^(?P<query>\w+)/$', views.search2, name='search2'),
+    url(r'^(?P<query>\w+)/$', views.search, name='search'),
+    url(r'^wildcard/(?P<query>\w+)/$', views.wildcard, name='wildcard'),
 )
