ee939492b2428914d5eec7c39a9c68332ff732d0
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-21 15:26:42 +0000

    tidy and change for wildcards at start and end of queries

--- django_template/local_apps/es/views.py
+++ django_template/local_apps/es/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import json, requests
+import json, requests, re
 #from django.views.decorators.cache import cache_page
 
 def wildcard(request, query):
-    data = { "query": { "wildcard" : { "ID" : query+"*" } } }  
+    query = query.replace("w", "*")
+    data = { "query": { "wildcard" : { "ID" : query } } }  
     context = _getContext(data)
     return render(request, 'search/elasticsearch.html', context)
 
@@ -13,6 +14,10 @@ def search(request, query):
     context = _getContext(data)
     return render(request, 'search/elasticsearch.html', context, content_type='text/html')
 
+'''
+Query the elasticsearch server for given search data and return the
+context dictionary to pass to the template
+'''
 def _getContext(data):
     size = 20;
     response = requests.post('http://127.0.0.1:9200/dbsnp142/_search?size='+str(size), data=json.dumps(data))
@@ -22,7 +27,11 @@ def _getContext(data):
     content = []
     if(len(response.json()['hits']['hits']) >= 1):
         for hit in response.json()['hits']['hits']:
+            #print(hit['_source']['INFO'])
+            _addInfo(content, hit)
             content.append(hit['_source'])
+            #print(hit['_source'])
+
     context["data"] = content
     context["total"] = response.json()['hits']['total']
     if(int(response.json()['hits']['total']) < size):
@@ -30,3 +39,15 @@ def _getContext(data):
     else:
         context["size"] = size
     return context;
+
+'''
+Split and add INFO tags and values
+'''
+def _addInfo(content, hit):
+    infos = re.split(';', hit['_source']['INFO'])
+    for info in infos:
+        if "=" in info:
+            parts = re.split('=', info)
+            hit['_source'][parts[0]] = parts[1]
+        else:
+            hit['_source'][info] = ""
