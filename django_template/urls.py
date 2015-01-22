e55cda599156f9b1407b93c78e89d6a1dbe7d93c
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-22 16:21:52 +0000

    update links

--- django_template/urls.py
+++ django_template/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'django_template.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^bands/', include('bands.urls', namespace="bands")),
+    url(r'^', include('bands.urls', namespace="bands")),
     url(r'^api/', include(api.urls)),
     url(r'^search/', include('es.urls', namespace="es")),
 )
