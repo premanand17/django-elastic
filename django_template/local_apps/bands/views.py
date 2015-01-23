642724236edfa44d305b1f5e1c0ff46c7094f8ac
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-23 12:47:16 +0000

    order src chromosomes

--- django_template/local_apps/bands/views.py
+++ django_template/local_apps/bands/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.cache import cache_page
 
 from db.models import Feature, Featureloc, Featureprop, Cv, Cvterm
 
-import logging
+import logging, re
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -19,7 +19,11 @@ def cytobands(request, org):
     #bands = Featureloc.objects.getCytoBands(org)
 
     srcfeatureIds = Featureloc.objects.getSrcFeatures(org)
-    srcfeatures = Feature.objects.filter(featureloc_srcfeature=srcfeatureIds).order_by('feature_id')
+    srcfeatures = Feature.objects.filter(featureloc_srcfeature=srcfeatureIds)
+    
+    # order by chr number  (assumes chr prefix)
+    srcfeatures = sorted(srcfeatures, key=lambda srcfeature: (int(srcfeature.uniquename.replace("chr", ""))
+                         if srcfeature.uniquename.replace("chr", "").isnumeric() else 1000))
 
     cv = Cv.objects.get(name="DIL")
     cvtermDIL = Cvterm.objects.filter(cv=cv)
@@ -34,7 +38,11 @@ def cytobands(request, org):
 def cytobands2(request, org):
     bands = Featureloc.objects.getCytoBands(org)
     srcfeatureIds = bands.distinct('srcfeature_id')
-    srcfeatures = Feature.objects.filter(featureloc_srcfeature=srcfeatureIds).order_by('feature_id')
+    srcfeatures = Feature.objects.filter(featureloc_srcfeature=srcfeatureIds)
+    
+    # order by chr number (assumes chr prefix)
+    srcfeatures = sorted(srcfeatures, key=lambda srcfeature: (int(srcfeature.uniquename.replace("chr", ""))
+                         if srcfeature.uniquename.replace("chr", "").isnumeric() else 1000))
 
     cv = Cv.objects.get(name="DIL")
     cvtermDIL = Cvterm.objects.filter(cv=cv)
