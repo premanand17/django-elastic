2d5446a83e0827aebc2d1752ff4bc2182fe000c6
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-26 15:01:03 +0000

    more tests

--- django_template/local_apps/es/tests/tests_es.py
+++ django_template/local_apps/es/tests/tests_es.py
@@ -13,3 +13,25 @@ class EsTest(TestCase):
         resp = requests.get(settings.ELASTICSEARCH_URL+'/_cluster/health')
         self.assertEqual(resp.status_code, 200, "Health page status code")
         self.assertFalse(resp.json()['status'] == 'red', "Elasticsearch status check")
+
+    def test_snp_search(self):
+        resp = self.client.get('/search/rs333/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('data' in resp.context)
+        snp = resp.context['data'][0]
+        self._SNPtest(snp)
+        
+    def test_snp_wildcard(self):
+        resp = self.client.get('/search/wildcard/rs33311w/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('data' in resp.context)
+
+        for snp in resp.context['data']:
+            self._SNPtest(snp)
+            
+    def _SNPtest(self, snp):
+        self.assertTrue(snp['POS'])
+        self.assertTrue(snp['ID'])
+        self.assertTrue(snp['REF'])
+        self.assertTrue(snp['ALT'])
+        self.assertTrue(snp['SRC'])
