6977775e73c2612a8213014d61b144a53ae05863
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-27 11:06:14 +0000

    add home page

--- django_template/local_apps/bands/urls.py
+++ django_template/local_apps/bands/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns( 'bands',
     url(r'^cvlist/$', views.cvlist, name='cvlist'),
     url(r'^ws/(?P<org>\w+)/$', views.cytobands_ws, name='cytobands_ws'),
     url(r'^(?P<org>[human|mouse]\w+)/$', views.cytobands, name='cytobands'),
+    url(r'^$', views.cytobandsHuman, name='cytobandsHuman'),
 )
