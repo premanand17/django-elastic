9ed2e3d34b36bf9e5d68da77185a16e86656fb8d
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2015-01-27 11:07:02 +0000

    tidy and comment

--- django_template/urls.py
+++ django_template/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from db.api import CvtermResource, CvtermFullResource, CvResource, CvtermpropResource, FeaturelocResource, FeaturelocFullResource, FeatureResource, FeatureFullResource, FeaturepropResource, FeaturepropFullResource, OrganismResource
 from tastypie.api import Api
 
+# register tastypie api
 api = Api(api_name='dev')
 api.register(CvtermResource())
 api.register(CvtermFullResource())
@@ -18,10 +19,6 @@ api.register(CvtermpropResource())
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_template.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('bands.urls', namespace="bands")),
     url(r'^api/', include(api.urls)),
     url(r'^search/', include('es.urls', namespace="es")),
