b9e700ef86aa9667dad5680d2f19ecabece646ad
tcarver <tjc29@duffield>
2015-02-09 14:14:52 +0000

    coding style

--- django_template/local_apps/es/urls.py
+++ django_template/local_apps/es/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 from es import views
 
-urlpatterns = patterns( 'es',
-    url(r'^(?P<query>\w+)/$', views.search, name='search'),
-    url(r'^wildcard/(?P<query>\w+)/$', views.wildcard, name='wildcard'),
-)
+urlpatterns = patterns('es',
+                       url(r'^(?P<query>\w+)/$', views.search, name='search'),
+                       url(r'^wildcard/(?P<query>\w+)/$', views.wildcard,
+                           name='wildcard'),
+                       )
