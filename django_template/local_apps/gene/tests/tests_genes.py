028e4f80e287425f752783392380d4ab0feb01fc
tcarver <tjc29@duffield>
2015-02-16 16:39:25 +0000

    add setUp()

--- django_template/local_apps/gene/tests/tests_genes.py
+++ django_template/local_apps/gene/tests/tests_genes.py
@@ -1,8 +1,23 @@
 from django.test import TestCase
+from db.models import Feature, Cv, Cvterm, Organism, Db, Dbxref
 
 
 class GenesTestCase(TestCase):
 
+    def setUp(self):
+        cv = Cv.objects.create(name='sequence')
+        db = Db.objects.create(name='null')
+        dbxref = Dbxref.objects.create(db_id=db.db_id, accession='gene')
+        termtype = Cvterm(dbxref_id=dbxref.dbxref_id, cv_id=cv.cv_id,
+                          name='gene', definition='gene',
+                          is_obsolete=0, is_relationshiptype=0)
+        termtype.save()
+        organism = Organism.objects.create(common_name='human_GRCh38')
+        self.feature = Feature(organism=organism, name='PTPN22',
+                               uniquename='PTPN22',
+                               type=termtype, is_analysis=0, is_obsolete=0)
+        self.feature.save()
+
     def test_genes(self):
-        resp = self.client.get('/genes/PTPN22/')
+        resp = self.client.get('/gene/PTPN22/')
         self.assertEqual(resp.status_code, 200)
