a974239d6a4f4394a63caa9294e85eb535c14a47
tcarver <tjc29@duffield>
2015-02-16 14:46:08 +0000

    update comment

--- django_template/local_apps/gene/templatetags/gene_tags.py
+++ django_template/local_apps/gene/templatetags/gene_tags.py
@@ -6,7 +6,8 @@ register = template.Library()
 
 @register.inclusion_tag('gene/gene_section.html')
 def show_gene_section(gene_feature):
-    ''' Template tag to render a gene section given a chado gene feature. '''
+    ''' Template inclusion tag to render a gene section given a
+    chado gene feature. '''
 
     feature_dbxrefs = FeatureDbxref.objects.filter(feature=gene_feature)
     gene_dbxrefs = []
