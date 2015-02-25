1cbed25112f9a67e9aa97184f19cdbd507941bec
tcarver <tjc29@duffield>
2015-02-25 10:43:06 +0000

    updates

--- django_template/wsgi.py
+++ django_template/wsgi.py
@@ -1,14 +1,11 @@
-"""
-WSGI config for django_template project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_template.settings")
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+path = os.path.join(os.path.dirname(__file__), "../")
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_template.settings'
+
 application = get_wsgi_application()
