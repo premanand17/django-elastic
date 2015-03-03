6fa7c70cfff0d2a34b0bd533e1920ccc3ab79e3f
tcarver <tjc29@duffield>
2015-03-03 14:24:26 +0000

    provide page title

--- django_template/local_apps/region/views.py
+++ django_template/local_apps/region/views.py
@@ -7,6 +7,7 @@ def region_page(request, region):
     ''' Region search'''
     data = {"query": {"match": {"attr.region_id": region}}}
     context = elastic_search(data, db=settings.REGIONDB)
+    context['title'] = "Region"
     print(context)
     return render(request, 'region/region.html', context,
                   content_type='text/html')
