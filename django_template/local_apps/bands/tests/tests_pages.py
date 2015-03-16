09a1878e26b31b0b492e1aad606f9061d2be2c2a
tcarver <tjc29@duffield>
2015-03-16 14:30:08 +0000

    set up fixtures for all test methods

--- django_template/local_apps/bands/tests/tests_pages.py
+++ django_template/local_apps/bands/tests/tests_pages.py
@@ -1,14 +1,32 @@
 from django.test import TestCase
+from django.core.management import call_command
+import string
+import random
 
 
 class BandsViewsTestCase(TestCase):
-    """Load fixture"""
-    fixtures = ['db2.json']
+
+    @classmethod
+    def setUpClass(cls):
+        ''' Load fixture for all test methods in this class '''
+        super(BandsViewsTestCase, cls).setUpClass()
+        call_command('loaddata', 'db2.json', verbosity=0)
+
+    @classmethod
+    def tearDownClass(cls):
+        super(BandsViewsTestCase, cls).tearDownClass()
+        call_command('flush', verbosity=0, interactive=False)
 
     def test_band(self):
-        resp = self.client.get('/human_GRCh38/')
-        self.assertEqual(resp.status_code, 200)
+        ''' Test the home page '''
+        self._home_page(self.client.get('/'))
 
+    def test_band_uncached(self):
+        ''' Test the home page '''
+        self._home_page(self.client.get('/?'+self._id_generator()))
+
+    def _home_page(self, resp):
+        self.assertEqual(resp.status_code, 200)
         if resp.context is not None:  # if none it is cached
             self.assertEqual(resp.context['org'], 'human_GRCh38')
             self.assertTrue('srcfeatures' in resp.context)
@@ -19,10 +37,13 @@ class BandsViewsTestCase(TestCase):
         resp = self.client.get('/cvlist/')
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('cv_list' in resp.context)
-        cv = resp.context['cv_list'][0]
+        cv = resp.context['cv_list'][2]
         self.assertTrue(hasattr(cv, 'cv_id'))
         self.assertTrue(hasattr(cv, 'name'))
 
     def test_not_found(self):
         resp = self.client.get('/xxx/')
         self.assertEqual(resp.status_code, 404)
+
+    def _id_generator(self, size=6, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
