6c49117c34bd1bc31321306976c7c387d531f9da
tcarver <tjc29@duffield>
2015-03-23 09:34:25 +0000

    add GFF loader testing

--- django_template/local_apps/es/tests/settings_idx.py
+++ django_template/local_apps/es/tests/settings_idx.py
@@ -6,4 +6,5 @@ TEST_DATA_PATH = os.path.join(settings.BASE_DIR, 'django_template/local_apps/es/
 IDX = {'GENE': {'indexName': 'test__gene', 'indexGene': TEST_DATA_PATH+'genenames.org.test.txt.gz'},
        'DISEASE': {'indexName': 'test__disease', 'indexDisease': TEST_DATA_PATH+'disease.list'},
        'MARKER': {'indexName': 'test__marker', 'indexSNP': TEST_DATA_PATH+'dbsnp142_test.vcf.gz'},
+       'GFF_GENERIC': {'indexName': 'test__gff', 'indexType': 'gff', 'indexGFF': TEST_DATA_PATH+'test.gff.gz'},
        }
