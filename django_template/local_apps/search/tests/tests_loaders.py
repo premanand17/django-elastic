2996ed58f7f940c8c16f5d8821636f31e653a806
tcarver <tjc29@duffield>
2015-03-25 15:25:14 +0000

    rename es to search and tidy

--- django_template/local_apps/search/tests/tests_loaders.py
+++ django_template/local_apps/search/tests/tests_loaders.py
@@ -7,14 +7,14 @@ import time
 
 
 def setUpModule():
-    ''' Run the index loading script to create test indeces '''
+    ''' Run the index loading script to create test indices '''
     for idx_kwargs in IDX.values():
         call_command('index_search', **idx_kwargs)
     time.sleep(2)
 
 
 def tearDownModule():
-    ''' Remove loaded test indeces '''
+    ''' Remove loaded test indices '''
     for key in IDX:
         requests.delete(settings.SEARCH_ELASTIC_URL +
                         '/' + IDX[key]['indexName'])
