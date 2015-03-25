1249c40474049d8e9676161e7724e1c7c465b952
tcarver <tjc29@duffield>
2015-03-25 11:14:17 +0000

    tidy

--- django_template/local_apps/es/urls.py
+++ django_template/local_apps/es/urls.py
@@ -2,14 +2,11 @@ from django.conf.urls import patterns, url
 from es import views
 
 urlpatterns = patterns('es',
-                       url(r'^(?P<query>[\.\w*]+)/$', views.search,
-                           name='search'),
+                       url(r'^(?P<query>[\.\w*]+)/$', views.search, name='search'),
                        url(r'^(?P<src>\w+):(?P<start>[\w]+)-(?P<stop>[\w]+)/$',
                            views.range_overlap_search, name='range_search'),
-                       url(r'^(?P<src>\w+):(?P<start>[\w]+)-(?P<stop>[\w]+)/db/(?P<search_db>[\w]+)$',  # @IgnorePep8
-                           views.range_overlap_search,
-                           name='filtered_range_search'),
+                       url(r'^(?P<src>\w+):(?P<start>[\w]+)-(?P<stop>[\w]+)/db/(?P<search_db>[\w]+)$',
+                           views.range_overlap_search, name='filtered_range_search'),
                        url(r'^(?P<query>[\.\w*]+)/db/(?P<search_db>[\w]+)$',
-                           views.search,
-                           name='filtered_search')
+                           views.search, name='filtered_search')
                        )
