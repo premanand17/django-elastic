9f227f02907b41330b70142e429b465e501b3814
prem <premanand.achuthan@cimr.cam.ac.uk>
2015-04-01 13:37:36 +0100

    remove django_template added in error

--- django_template/local_apps/gene/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from gene import views
-
-urlpatterns = patterns('genes',
-                       url(r'^(?P<gene>[-\w]+)/$',
-                           views.gene_page, name='gene_page'),
-                       )
--- django_template/local_apps/marker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from marker import views
-
-urlpatterns = patterns('marker',
-                       url(r'^(?P<marker>[-\w]+)/$', views.marker_page,
-                           name='marker_page'),
-                       )
