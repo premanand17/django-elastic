6185b8025a37628d6e3d89dc4ca0a5f9865651e1
tcarver <tjc29@duffield>
2015-04-10 14:36:01 +0100

    remove reverse_proxy and add ajax views

--- search/urls.py
+++ search/urls.py
@@ -1,11 +1,21 @@
 from django.conf.urls import url
 from search import views
 
-urlpatterns = [url(r'^(?P<query>[\.\w*]+)/$', views.search, name='search'),
+urlpatterns = [
+               # range overlap queries @IgnorePep8
                url(r'^(?P<src>\w+):(?P<start>[\w]+)-(?P<stop>[\w]+)/$',
                    views.range_overlap_search, name='range_search'),
                url(r'^(?P<src>\w+):(?P<start>[\w]+)-(?P<stop>[\w]+)/db/(?P<search_db>[\w]+)$',
                    views.range_overlap_search, name='filtered_range_search'),
+
+               # query string
+               url(r'^(?P<query>[\.\w*]+)/$', views.search, name='search'),
                url(r'^(?P<query>[\.\w*]+)/db/(?P<search_db>[\w]+)$',
-                   views.search, name='filtered_search')
+                   views.search, name='filtered_search'),
+
+               # count queries
+               url(r'^(?P<query>[\.\w*]+)/db/(?P<search_db>[\w,]+)/(?P<ajax>[\w]+)',
+                   views.ajax_search, name='count'),
+               url(r'^(?P<src>\w+):(?P<start>[\w]+)-(?P<stop>[\w]+)/db/(?P<search_db>[\w,]+)/(?P<ajax>[\w]+)',
+                   views.ajax_range_overlap_search, name='filtered_range_search'),
                ]
