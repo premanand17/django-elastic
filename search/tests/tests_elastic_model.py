ea387aa9298a25f913132e4dbaa9b1df893ae984
tcarver <tjc29@duffield>
2015-04-15 15:52:33 +0100

    update query tests

--- search/tests/tests_elastic_model.py
+++ search/tests/tests_elastic_model.py
@@ -2,9 +2,9 @@ from django.test import TestCase, override_settings
 from django.core.management import call_command
 from search.tests.settings_idx import IDX
 import requests
-from search.elastic_model import Elastic, QueryBool, Query, BuildQuery
+from search.elastic_model import Elastic, QueryBool, Query, ElasticQuery
 from search.elastic_settings import ElasticSettings
-import json
+import time
 
 
 @override_settings(SEARCH={'default': {'IDX': {'DEFAULT': IDX['MARKER']['indexName']},
@@ -12,6 +12,7 @@ import json
 def setUpModule():
     ''' Load test indices (marker) '''
     call_command('index_search', **IDX['MARKER'])
+    time.sleep(1)
 
 
 @override_settings(SEARCH={'default': {'IDX': {'DEFAULT': IDX['MARKER']['indexName']},
@@ -40,11 +41,19 @@ class ElasticModelTest(TestCase):
         mapping = elastic.get_mapping('marker/xx')
         self.assertTrue('error' in mapping, "Database name in mapping result")
 
-    def test_build_query(self):
+    def test_filtered_query(self):
         query_bool = QueryBool()
-        query_bool.must([{"term": {"id": "rs2476601"}}])
+        query_bool.must([{"term": {"id": "rs373328635"}}])
         query_bool.must_not([{"term": {"seqid": "2"}}])
-        query_bool.should({"range": {"start": {"gte": 113834945}}})
-        query_bool.should([{"range": {"start": {"gte": 113834944}}}])
-        query = BuildQuery.filtered_bool(Query.match_all(), query_bool, sources=["id", "seqid"])
-        print("curl 'http://jenkins:9200/dbsnp142/_search?pretty=true' -d '"+json.dumps(query.query)+"'")
+        query_bool.should({"range": {"start": {"gte": 10054}}})
+        query_bool.should([{"range": {"start": {"gte": 10050}}}])
+        query = ElasticQuery.filtered_bool(Query.match_all(), query_bool, sources=["id", "seqid"])
+        elastic = Elastic(query, db=ElasticSettings.idx('DEFAULT'))
+        self.assertTrue(elastic.get_result()['total'] == 1, "Elastic filtered query retrieved marker (rs373328635)")
+
+    def test_string_query(self):
+        query_term = "rs2476601"
+        fields = ["id"]
+        query = ElasticQuery.query_string(query_term, fields)
+        elastic = Elastic(query, db=ElasticSettings.idx('DEFAULT'))
+        self.assertTrue(elastic.get_result()['total'] == 1, "Elastic string query retrieved marker (rs2476601)")
