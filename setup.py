c6aad5fd24df0f45a98b869d8c8b9cfd397aff47
tcarver <tcarver@users.noreply.github.com>
2015-04-28 15:00:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name='elastic',
+    name='django-elastic',
     version='0.1a1',
     packages=find_packages(),
     include_package_data=True,
