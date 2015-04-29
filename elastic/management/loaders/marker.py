79b0e69f3beae749910013a1df453f7ce2d7da1b
tcarver <tjc29@duffield>
2015-04-29 10:57:11 +0100

    Overrides Delimeter.parse_line() - to prefix rs numbers with "rs"

--- elastic/management/loaders/marker.py
+++ elastic/management/loaders/marker.py
@@ -37,14 +37,29 @@ class RsMerge(DelimeterLoader):
     def _create_rs_merge_mapping(self, **options):
         ''' Create the mapping for rs index '''
         props = MappingProperties('rs_merge')
-        props.add_property("rshigh", "integer", index="not_analyzed")
-        props.add_property("rslow", "integer", index="not_analyzed")
+        props.add_property("rshigh", "string", index="not_analyzed")
+        props.add_property("rslow", "string", index="not_analyzed")
         props.add_property("build_id", "integer", index="no")
         props.add_property("orien", "integer", index="no")
         props.add_property("create_time", "date", index="no", property_format="yyyy-MM-dd HH:mm:ss.SSS")
         props.add_property("last_updated_time", "date", index="no", property_format="yyyy-MM-dd HH:mm:ss.SSS")
-        props.add_property("rscurrent", "integer", index="not_analyzed")
+        props.add_property("rscurrent", "string", index="not_analyzed")
         props.add_property("orien2current", "string", index="no")
         props.add_property("notes", "string", index="no")
         self.mapping(props, 'rs_merge', **options)
         return props
+
+    def parse_line(self, parts, column_names, idx_name, idx_type, is_GFF, is_GTF):
+        ''' Overrides Delimeter.parse_line() - to prefix rs numbers with "rs". '''
+        doc_data = {}
+        for idx, p in enumerate(parts):
+            p = p.strip()
+            if column_names[idx] in ["rscurrent", "rslow", "rshigh"]:
+                p = "rs"+p
+            if self.is_str(column_names[idx], idx_name, idx_type):
+                doc_data[column_names[idx]] = p
+            elif p.isdigit():
+                doc_data[column_names[idx]] = int(p)
+            else:
+                doc_data[column_names[idx]] = p
+        return doc_data
