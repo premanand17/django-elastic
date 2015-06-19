c2236a78d59a72cdd09606bed4580a74398d80f4
tcarver <tjc29@duffield>
2015-06-19 11:25:38 +0100

    if an _id is povide use that otherwise auto generate

--- elastic/management/loaders/loader.py
+++ elastic/management/loaders/loader.py
@@ -193,7 +193,11 @@ class JSONLoader(Loader):
         line_num = 0
         try:
             for row in raw_json_data:
-                json_data += '{"index": {}}\n'
+                if '_id' in row:
+                    json_data += '{"index": {"_id": "%s"}}\n' % row['_id']
+                    del row['_id']
+                else:
+                    json_data += '{"index": {}}\n'
                 json_data += json.dumps(row) + '\n'
                 line_num += 1
 
