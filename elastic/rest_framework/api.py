5b8c45caf191fb5cb3230f219bb3e046241775dc
tcarver <tjc29@duffield>
2015-07-03 14:26:45 +0100

    update fields

--- elastic/rest_framework/api.py
+++ elastic/rest_framework/api.py
@@ -3,22 +3,23 @@ from elastic.rest_framework.resources import ListElasticMixin
 
 
 class PublicationSerializer(serializers.Serializer):
+    ''' Publication serializer '''
 
     class PublicationAuthor(serializers.Serializer):
-        ForeName = serializers.StringRelatedField()
-        LastName = serializers.StringRelatedField()
-        Initials = serializers.StringRelatedField()
+        ForeName = serializers.ReadOnlyField()
+        LastName = serializers.ReadOnlyField()
+        Initials = serializers.ReadOnlyField()
 
     PMID = serializers.IntegerField()
-    title = serializers.StringRelatedField()
-    abstract = serializers.StringRelatedField()
-    journal = serializers.StringRelatedField()
+    title = serializers.ReadOnlyField()
+    abstract = serializers.ReadOnlyField()
+    journal = serializers.ReadOnlyField()
     authors = serializers.ListField(child=PublicationAuthor())
     date = serializers.DateField()
-    tags__disease = serializers.ListField(child=serializers.StringRelatedField())
+    tags = serializers.DictField()
 
 
 class PublicationViewSet(ListElasticMixin, viewsets.GenericViewSet):
     serializer_class = PublicationSerializer
     idx = 'publications_v0.0.1'
-    filter_fields = ('PMID', 'title', 'authors__LastName')
+    filter_fields = ('PMID', 'title', 'authors__LastName', 'tags__disease')
