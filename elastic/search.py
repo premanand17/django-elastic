d91ac2f62d321c6847e4f08ee80e6e4fabf83ac5
tcarver <tim.carver@cimr.cam.ac.uk>
2015-11-03 14:17:07 +0000

    add to suggester error

--- elastic/search.py
+++ elastic/search.py
@@ -308,7 +308,7 @@ class Suggest(object):
         response = Search.elastic_request(elastic_url, url, data=json.dumps(suggest))
         logger.debug("curl -XPOST '" + elastic_url + '/' + url + "' -d '" + json.dumps(suggest) + "'")
         if response.status_code != 200:
-            logger.warn("Error: elastic response 200:" + url)
+            logger.warn("Suggeter Error: elastic response 200:" + url)
             logger.warn(response.json())
         return response.json()
 
